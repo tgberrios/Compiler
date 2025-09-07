985966f7d926ad7f39fb45e09f2463c900ad6c56
Tomy Gustavo Berrios Berrios <tgberrios@outlook.com>
2025-09-06 22:19:25 -0600

    Data Governance System

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
+#include "DataGovernance.h"
 #include "StreamingData.h"
 #include <chrono>
 #include <iostream>
 #include <thread>
 
 int main() {
+  Logger::initialize();
+  Logger::info("main", "Starting DataSync system with Data Governance");
+
+  DataGovernance dg;
+  dg.initialize();
+  dg.runDiscovery();
+  dg.generateReport();
+
   StreamingData sd;
   sd.run();
   return 0;
