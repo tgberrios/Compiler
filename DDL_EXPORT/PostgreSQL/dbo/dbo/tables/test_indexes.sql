f6b3f0c6a5f4ea2933edc1e2f76accd8f7f27899
Tomy Gustavo Berrios Berrios <tgberrios@outlook.com>
2025-09-08 01:09:14 -0600

    working

--- DDL_EXPORT/PostgreSQL/dbo/dbo/tables/test_indexes.sql
+++ DDL_EXPORT/PostgreSQL/dbo/dbo/tables/test_indexes.sql
@@ -1,6 +1,6 @@
 -- Table DDL for dbo.test_indexes
 -- Engine: PostgreSQL
 -- Database: dbo
--- Generated: 1757221512
+-- Generated: 1757222426
 
 CREATE TABLE "dbo"."test_indexes" (id integer NOT NULL, name character varying(100), created_at timestamp without time zone);
--- DDL_EXPORT/PostgreSQL/dbo/dbo/tables/test_multiple_changes.sql
+++ DDL_EXPORT/PostgreSQL/dbo/dbo/tables/test_multiple_changes.sql
@@ -1,6 +1,6 @@
 -- Table DDL for dbo.test_multiple_changes
 -- Engine: PostgreSQL
 -- Database: dbo
--- Generated: 1757221512
+-- Generated: 1757222426
 
 CREATE TABLE "dbo"."test_multiple_changes" (id integer NOT NULL, name character varying(100), email character varying(100), created_at timestamp without time zone);
